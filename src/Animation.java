32dac9fc001f66272f2762051189d92e1aba6497
naoi9 <zeal24360135@fsgcl.jp>
2025-06-02 10:41:36 +0900

    オーマイゴシゴシver5.2

---
+++ src/Animation.java
@@ -0,0 +1,125 @@
+
+//public class Animation extends MyFrame{
+//	public void run() {
+//		int x=30;
+//		int y=100;
+//		while(true)
+//		{
+//		while(x<=170){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=5;
+//			sleep(0.01);
+//			
+//		}
+//		while(x>=30){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x-=5;
+//			sleep(0.01);
+//			
+//		}
+//		}
+//
+//}
+////5-1
+//public class Animation extends MyFrame{
+//	public void run() {
+//		int x=30;
+//		int y=100;
+//		while(y>=30){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			y-=5;
+//			sleep(0.01);
+//			
+//		}
+//		while(x<=170){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=5;
+//			sleep(0.01);
+//			
+//		}
+//		}
+//
+//}
+//5-2
+public class Animation extends MyFrame{
+	public void run() {
+		int x=30;
+		int y=100;
+		while(x<=300 & y<=300){
+			clear();;
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
+			y+=5;
+			sleep(0.01);
+			
+		}
+		while(x>=30){
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x-=5;
+			sleep(0.01);
+			
+			while(x<=300 & y>=30){
+				clear();
+				setColor(0,128,0);
+				fillRect(x,y,10,100);
+				x+=5;
+				y-=5;
+				sleep(0.01);
+				
+			
+			}
+
+		
+		}
+		while(x<=300 & y>=30){
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x+=5;
+			y-=5;
+			sleep(0.01);
+			
+		
+		}
+
+
+}
+}
+//5-3
+//public class Animation extends MyFrame{
+//	public void run() {
+//		int x=30;
+//		int y=100;
+//		for(int i=1;i<=3;i=i+1)
+//		{
+//		while(x<=170){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x+=5;
+//			sleep(0.01);
+//			
+//		}
+//		while(x>=30){
+//			clear();
+//			setColor(0,128,0);
+//			fillRect(x,y,10,100);
+//			x-=5;
+//			sleep(0.01);
+//			
+//		}
+//		}
+//     }
+//}
+
--- src/Start.java
+++ src/Start.java
@@ -4,12 +4,14 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("aaaa");
-		//MyFrame frame1=new RectTest();
-		//MyFrame frame1=new OvalTest();
-		//MyFrame frame1=new TextTest();
-		//MyFrame frame1=new RectAndOval();
+//		MyFrame frame1=new RectTest();
+//		MyFrame frame1=new OvalTest();
+//		MyFrame frame1=new TextTest();
+//		MyFrame frame1=new RectAndOval();
 //		MyFrame frame1=new RectLoop();
-		//MyFrame frame1=new RectIf();
-		MyFrame frame1=new 練習問題4();
+//		MyFrame frame1=new RectIf();
+//		MyFrame frame1=new 練習問題4();
+//		MyFrame frame1=new DisplayRectsSlow();
+		MyFrame frame1=new Animation();
 	}
 }
